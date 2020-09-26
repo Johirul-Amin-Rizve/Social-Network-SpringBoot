f146fb4a4e5f2266bebb7aafc3ac7cb81177ac63
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-09-26 20:04:02 +0600

    user registration fix

--- src/main/java/com/goruslan/socialgeeking/service/UserService.java
+++ src/main/java/com/goruslan/socialgeeking/service/UserService.java
@@ -25,49 +25,16 @@ public class UserService {
     }
 
     public User register(User user) {
-        // Take the password from the form and encode
         String secret = "{bcrypt}" + encoder.encode(user.getPassword());
         user.setPassword(secret);
-
-        /* Confirm password
-            - Setting user's pass to encrypted pass. When we save it, @PasswordMatch annotation will throw validation constraint error.
-            - If validation constraint error exists, 'save' method won't work. */
+        user.setEnabled(true);
         user.setConfirmPassword(secret);
-
-        // Assigning a role to the user
         user.addRole(roleService.findByName("ROLE_USER"));
-
-        // Set an activation code
-
-        // Disable the user before saving.
-
-        // Save the user.
         save(user);
-
-        // Send activation email.
-        sendActivationEmail(user);
-
-        // Return user.
-
         return user;
     }
 
     public User save(User user) {
         return userRepository.save(user);
     }
-
-
-    @Transactional
-    public void saveUsers(User... users) {
-        for(User user : users) {
-            logger.info("Saving User: " + user.getEmail());
-            userRepository.save(user);
-        }
-    }
-
-    public void sendActivationEmail(User user) {
-        // Send the email
-
-    }
-
 }
