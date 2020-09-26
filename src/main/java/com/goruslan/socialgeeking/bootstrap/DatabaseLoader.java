e996ab03248e2a6c216633c6f812bf4bf8acb53b
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-09-26 19:26:08 +0600

    demo application initial commit 2

--- src/main/java/com/goruslan/socialgeeking/bootstrap/DatabaseLoader.java
+++ src/main/java/com/goruslan/socialgeeking/bootstrap/DatabaseLoader.java
@@ -41,45 +41,45 @@ public class DatabaseLoader implements CommandLineRunner {
 
         // add users and roles
         addUserAndRoles();
-        Map<String,String> posts = new HashMap<>();
-        posts.put("Securing Spring Boot APIs and SPAs with OAuth 2.0","https://auth0.com/blog/securing-spring-boot-apis-and-spas-with-oauth2/?utm_source=reddit&utm_medium=sc&utm_campaign=springboot_spa_securing");
-        posts.put("Easy way to detect Device in Java Web Application using Spring Mobile - Source code to download from GitHub","https://www.opencodez.com/java/device-detection-using-spring-mobile.htm");
-        posts.put("Tutorial series about building microservices with SpringBoot (with Netflix OSS)","https://medium.com/@marcus.eisele/implementing-a-microservice-architecture-with-spring-boot-intro-cdb6ad16806c");
-        posts.put("Detailed steps to send encrypted email using Java / Spring Boot - Source code to download from GitHub","https://www.opencodez.com/java/send-encrypted-email-using-java.htm");
-        posts.put("Build a Secure Progressive Web App With Spring Boot and React","https://dzone.com/articles/build-a-secure-progressive-web-app-with-spring-boo");
-        posts.put("Building Your First Spring Boot Web Application - DZone Java","https://dzone.com/articles/building-your-first-spring-boot-web-application-ex");
-        posts.put("Building Microservices with Spring Boot Fat (Uber) Jar","https://jelastic.com/blog/building-microservices-with-spring-boot-fat-uber-jar/");
-        posts.put("Spring Cloud GCP 1.0 Released","https://cloud.google.com/blog/products/gcp/calling-java-developers-spring-cloud-gcp-1-0-is-now-generally-available");
-        posts.put("Simplest way to Upload and Download Files in Java with Spring Boot - Code to download from Github","https://www.opencodez.com/uncategorized/file-upload-and-download-in-java-spring-boot.htm");
-        posts.put("Add Social Login to Your Spring Boot 2.0 app","https://developer.okta.com/blog/2018/07/24/social-spring-boot");
-        posts.put("File download example using Spring REST Controller","https://www.jeejava.com/file-download-example-using-spring-rest-controller/");
-
-        posts.forEach((k,v) -> {
-            User u1 = users.get("user@gmail.com");
-            User u2 = users.get("super@gmail.com");
-            Post post = new Post(k, v);
-            if(k.startsWith("Build")) {
-                post.setUser(u1);
-            } else {
-                post.setUser(u2);
-            }
-
-
-            postRepository.save(post);
-
-            Comment spring = new Comment("Thank you for this link related to Spring Boot. I love it, great post!", post);
-            Comment security = new Comment("I love that you're talking about Spring Security", post);
-            Comment pwa = new Comment("What is this Progressive Web App thing all about? PWAs sound really cool.", post);
-            Comment comments[] = {spring,security,pwa};
-            for(Comment comment : comments) {
-                commentRepository.save(comment);
-                post.addComment(comment);
-            }
-
-        });
-
-        long postCount = postRepository.count();
-        System.out.println("Number of posts in the database: " + postCount );
+//        Map<String,String> posts = new HashMap<>();
+//        posts.put("Securing Spring Boot APIs and SPAs with OAuth 2.0","https://auth0.com/blog/securing-spring-boot-apis-and-spas-with-oauth2/?utm_source=reddit&utm_medium=sc&utm_campaign=springboot_spa_securing");
+//        posts.put("Easy way to detect Device in Java Web Application using Spring Mobile - Source code to download from GitHub","https://www.opencodez.com/java/device-detection-using-spring-mobile.htm");
+//        posts.put("Tutorial series about building microservices with SpringBoot (with Netflix OSS)","https://medium.com/@marcus.eisele/implementing-a-microservice-architecture-with-spring-boot-intro-cdb6ad16806c");
+//        posts.put("Detailed steps to send encrypted email using Java / Spring Boot - Source code to download from GitHub","https://www.opencodez.com/java/send-encrypted-email-using-java.htm");
+//        posts.put("Build a Secure Progressive Web App With Spring Boot and React","https://dzone.com/articles/build-a-secure-progressive-web-app-with-spring-boo");
+//        posts.put("Building Your First Spring Boot Web Application - DZone Java","https://dzone.com/articles/building-your-first-spring-boot-web-application-ex");
+//        posts.put("Building Microservices with Spring Boot Fat (Uber) Jar","https://jelastic.com/blog/building-microservices-with-spring-boot-fat-uber-jar/");
+//        posts.put("Spring Cloud GCP 1.0 Released","https://cloud.google.com/blog/products/gcp/calling-java-developers-spring-cloud-gcp-1-0-is-now-generally-available");
+//        posts.put("Simplest way to Upload and Download Files in Java with Spring Boot - Code to download from Github","https://www.opencodez.com/uncategorized/file-upload-and-download-in-java-spring-boot.htm");
+//        posts.put("Add Social Login to Your Spring Boot 2.0 app","https://developer.okta.com/blog/2018/07/24/social-spring-boot");
+//        posts.put("File download example using Spring REST Controller","https://www.jeejava.com/file-download-example-using-spring-rest-controller/");
+//
+//        posts.forEach((k,v) -> {
+//            User u1 = users.get("user@gmail.com");
+//            User u2 = users.get("super@gmail.com");
+//            Post post = new Post(k, v);
+//            if(k.startsWith("Build")) {
+//                post.setUser(u1);
+//            } else {
+//                post.setUser(u2);
+//            }
+//
+//
+//            postRepository.save(post);
+//
+//            Comment spring = new Comment("Thank you for this link related to Spring Boot. I love it, great post!", post);
+//            Comment security = new Comment("I love that you're talking about Spring Security", post);
+//            Comment pwa = new Comment("What is this Progressive Web App thing all about? PWAs sound really cool.", post);
+//            Comment comments[] = {spring,security,pwa};
+//            for(Comment comment : comments) {
+//                commentRepository.save(comment);
+//                post.addComment(comment);
+//            }
+//
+//        });
+//
+//        long postCount = postRepository.count();
+//        System.out.println("Number of posts in the database: " + postCount );
     }
 
 
