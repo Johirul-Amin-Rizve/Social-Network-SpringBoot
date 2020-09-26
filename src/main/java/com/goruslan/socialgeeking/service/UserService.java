c9885866dad4f7e155629a50139b639c1be5e151
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-09-26 20:15:38 +0600

    fix error

--- src/main/java/com/goruslan/socialgeeking/service/UserService.java
+++ src/main/java/com/goruslan/socialgeeking/service/UserService.java
@@ -6,9 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -24,30 +22,36 @@ public class UserService {
         encoder = new BCryptPasswordEncoder();
     }
 
-    @Transactional
     public User register(User user) {
         try {
             String secret = "{bcrypt}" + encoder.encode(user.getPassword());
             user.setPassword(secret);
-            user.setEnabled(true);
             user.setConfirmPassword(secret);
+            user.setEnabled(true);
             user.addRole(roleService.findByName("ROLE_USER"));
             save(user);
-            logger.debug(user.toString());
-        } catch (Exception e) {
-            logger.debug("Failed to create Booking " + e.getMessage());
+        }catch (Exception e){
+            logger.debug("Failed to register " + e.getMessage());
         }
         return user;
     }
 
-    @Transactional
     public User save(User user) {
         try {
             userRepository.save(user);
-            logger.debug(user.toString());
-        } catch (Exception e) {
-            logger.debug("Failed to create Booking " + e.getMessage());
+        } catch (Exception e){
+            logger.debug("Failed to save user " + e.getMessage());
         }
         return user;
     }
+
+
+    @Transactional
+    public void saveUsers(User... users) {
+        for(User user : users) {
+            logger.info("Saving User: " + user.getEmail());
+            userRepository.save(user);
+        }
+    }
+
 }
